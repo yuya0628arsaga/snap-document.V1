7701fd1dfd038df8c0db859da777a721c91a5ede
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-06 13:00:14 +0900

    Viteでビルドして初期画面表示

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>{{ 'タイトル' }} | {{ env('APP_NAME') }}</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" sizes="32x32" href="{{ asset('/images/favicon-32x32.ico') }}" type="image/x-icon">
+    @viteReactRefresh
+    @vite(['resources/sass/app.scss', 'resources/ts/app.tsx'])
+</head>
+
+<body>
+    <div id="test">test</div>
+</body>
+
+</html>
