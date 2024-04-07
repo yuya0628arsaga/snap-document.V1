74b77be1abd639263644fbc99e9d05959fff7d50
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-07 23:04:48 +0900

    timezoneと言語の設定

---
+++ lang/ja/documents.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+$tableLabel = 'ドキュメント';
+
+return [
+    'table_description' => "${tableLabel}テーブル",
+    'id' => "${tableLabel}ID",
+    'name' => 'ドキュメント名',
+    'url' => 'ドキュメントURL',
+];
