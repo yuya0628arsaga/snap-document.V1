b849a62aeff7158cf918b7a280a06793276cffe0
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-07 11:48:43 +0900

    chat画面のインタラクティブなUI作成

--- resources/ts/features/aiChatMessage/aiChatMessage.tsx
+++ resources/ts/features/aiChatMessage/aiChatMessage.tsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { createRoot } from 'react-dom/client'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from '@emotion/styled'
 import { bgColor, borderColor, fontWeight } from '../../utils/themeClient';
+import CircularProgress from '@mui/material/CircularProgress';
+
 import FormControl, { useFormControl } from '@mui/material/FormControl';
 import OutlinedInput from '@mui/material/OutlinedInput';
 
@@ -34,6 +36,10 @@ const MessageContainer = styled('div')`
     max-width: 80%;
     margin: 0 10%;
     height: 80vh;
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+        display:none;
+    }
 `
 
 const UsersQuestion = styled('div')`
@@ -53,11 +59,15 @@ const UsersQuestion = styled('div')`
         >.name {
             font-weight: ${fontWeight.bold};
             margin: 3px 0;
+            display: block;
         }
     }
 `
 
 const Load = styled('div')`
+    display: flex;
+    gap: 10px;
+    margin: 16px 0;
 `
 
 const AiAnswer = styled('div')`
@@ -65,7 +75,9 @@ const AiAnswer = styled('div')`
     background: ${bgColor.lightGray};
     display: flex;
     gap: 10px;
-    border-radius: 5px;
+    /* border-radius: 5px; */
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
     align-items: flex-start;
 
     > .icon {
@@ -77,16 +89,9 @@ const AiAnswer = styled('div')`
         >.name {
             font-weight: ${fontWeight.bold};
             margin: 3px 0;
+            display: block;
         }
     }
-`
-
-const FormContainer = styled('div')`
-    /* background: blue; */
-    margin: 50px;
-`
-const QuestionForm = styled('form')`
-    text-align: center;
 `
 
 const SendButton = styled('button')`
@@ -104,6 +109,15 @@ const InputText = styled('input')`
 `
 const AiChatMessage = (props) => {
 
+    const [inputQuestion, setInputQuestion] = useState('')
+    const [isGenerating, setIsGenerating] = useState(false)
+
+    const [displayedQuestion, setDisplayedQuestion] = useState('')
+    const [isDisplayQuestion, setIsDisplayQuestion] = useState(false)
+
+    const [answer, setAnswer] = useState('')
+    const [isDisplayChatGPT, setIsDisplayChatGPT] = useState(false)
+
     const fetch = () => {
         axios.get('http://localhost:80/api/test').then((res) => {
             console.log(res.data)
@@ -115,6 +129,34 @@ const AiChatMessage = (props) => {
         fetch()
     }
 
+    const handleChangeInput = (e) => {
+        setInputQuestion(e.target.value)
+    }
+
+    const sendQuestion = () => {
+        setIsGenerating(true)
+
+        setIsDisplayQuestion(true)
+        setDisplayedQuestion(inputQuestion)
+
+        setInputQuestion('')
+
+        setIsDisplayChatGPT(true)
+
+        axios.get('http://localhost:80/api/aaa').then((res) => {
+            console.log(res.data['message'])
+            setAnswer(res.data['message'])
+            setIsGenerating(false)
+        })
+
+        console.log(7777)
+        console.log(inputQuestion)
+
+        // setTimeout(() => {
+        //     setIsGenerating(false)
+        // }, 3000)
+
+    }
 
 
     return (
@@ -126,34 +168,55 @@ const AiChatMessage = (props) => {
                 <MainContainer>
                     <button onClick={handleClick}>ボタン</button>
                     <MessageContainer>
-                        <UsersQuestion>
-                            <div className="icon"><FaceOutlinedIcon style={{ color: `${borderColor.white}` }} /></div>
-                            <p className="text">
-                                <div className="name">You</div>
-                                回路エディタで素子を選択するにはどうしたらいいですか？
-                                Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
-                            </p>
-                        </UsersQuestion>
-                        <Load>
-                            <p>回答を生成中です...</p>
-                        </Load>
-                        <AiAnswer>
-                            <div className='icon'><SmartToyOutlinedIcon style={{ color: `${borderColor.white}` }} /></div>
-                            <p className="text">
-                                <div className="name">ChatGPT</div>
-                                Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
-                                Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
-                            </p>
-                        </AiAnswer>
+                        {isDisplayQuestion &&
+                            <UsersQuestion>
+                                <div className="icon"><FaceOutlinedIcon style={{ color: `${borderColor.white}` }} /></div>
+                                <p className="text">
+                                    <span className="name">You</span>
+                                    {/* 回路エディタで素子を選択するにはどうしたらいいですか？
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout." */}
+                                    { displayedQuestion }
+                                </p>
+                            </UsersQuestion>
+                        }
+                        {isGenerating &&
+                            <Load>
+                                <CircularProgress disableShrink size={25}/>
+                                <p>回答を生成中です...</p>
+                            </Load>
+                        }
+                        {isDisplayChatGPT &&
+                            <AiAnswer>
+                                <div className='icon'><SmartToyOutlinedIcon style={{ color: `${borderColor.white}` }} /></div>
+                                <p className="text">
+                                    <span className="name">ChatGPT</span>
+                                    { answer }
+                                    {/* Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."
+                                    Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout." */}
+                                </p>
+                            </AiAnswer>
+                        }
 
                     </MessageContainer>
 
-                    <FormContainer>
-                        <QuestionForm>
-                            <InputText className="test" type='text' placeholder='質問を入力してください。'></InputText>
-                            <SendButton type="submit"><SendIcon /></SendButton>
-                        </QuestionForm>
-                    </FormContainer>
+                    <div className='m_48'>
+                        <div className='ta_c'>
+                            <InputText onChange={handleChangeInput} value={inputQuestion} className="test" type='text' placeholder='質問を入力してください。'></InputText>
+                            <SendButton onClick={sendQuestion}><SendIcon /></SendButton>
+                        </div>
+                    </div>
                 </MainContainer>
             </Wrapper>
         </>
