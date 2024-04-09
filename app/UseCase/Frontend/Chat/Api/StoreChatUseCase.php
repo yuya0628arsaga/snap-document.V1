49db39390122a61ee6a97e9e524d01572d09048b
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-09 17:39:47 +0900

    トランザクション追加

--- app/UseCase/Frontend/Chat/Api/StoreChatUseCase.php
+++ app/UseCase/Frontend/Chat/Api/StoreChatUseCase.php
@@ -11,6 +11,7 @@ use App\Repositories\Frontend\Document\DocumentRepository;
 use App\Repositories\Frontend\Page\PageRepository;
 use App\Repositories\Frontend\Page\Params\StorePageParams;
 use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Sleep;
@@ -53,34 +54,34 @@ class StoreChatUseCase
             //     'answer' => $a.'コンテナが初めて起動されると、指定された名前の新しいデータベースが作成され、提供された構成変数で初期化されます。さらに、/docker-entrypoint-initdb.dにある拡張子.sh、.sql、.sql.gzのファイルがアルファベット順に実行されます。このディレクトリにSQLダンプをマウントすることで、簡単にmysqlサービスにデータを入れることができます。SQLファイルはデフォルトではMYSQL_DATABASE変数で指定されたデータベースにインポートされます。」'
             // ];
 
-            $document = $this->documentRepository->firstOrFailByDocumentName($documentName);
+            ['answer' => $answer, 'pdf_pages' => $pdfPages] = $res;
 
-            Log::debug($document);
+            DB::transaction(function () use ($question, $documentName, $answer, $pdfPages) {
+                $document = $this->documentRepository->firstOrFailByDocumentName($documentName);
 
-            $storeChatParams =
-                new StoreChatParams(
-                    question: $question,
-                    questionTokenCount: 12,
-                    // 'answer' => $res["answer"],
-                    answer: '回答回答回答',
-                    answerTokenCount: 28,
-                    date: CarbonImmutable::now(),
-                    userId: null,
-                    documentId: $document->id,
-                );
-
-            $chat = $this->chatRepository->create($storeChatParams);
-
-            // $pages = $res["pdf_pages"];
-            $pages = [1, 2, 3];
-            foreach ($pages as $page) {
-                $storePageParams =
-                    new StorePageParams(
-                        page: $page,
-                        chatId: $chat->id,
+                $storeChatParams =
+                    new StoreChatParams(
+                        question: $question,
+                        questionTokenCount: 12,
+                        answer: $answer,
+                        answerTokenCount: 28,
+                        date: CarbonImmutable::now(),
+                        userId: null,
+                        documentId: $document->id,
                     );
-                $this->pageRepository->create($storePageParams);
-            }
+
+                $chat = $this->chatRepository->create($storeChatParams);
+
+                foreach ($pdfPages as $pdfPage) {
+                    $storePageParams =
+                        new StorePageParams(
+                            page: $pdfPage,
+                            chatId: $chat->id,
+                        );
+                    // TODO::ここ insert で一括createできない？
+                    $this->pageRepository->create($storePageParams);
+                }
+            });
 
             return $res;
     }
