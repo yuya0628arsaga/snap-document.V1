45695644a055bbe71abac5c3bc1e7ab5b3e96458
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-11 10:57:35 +0900

    【サーバ】StoreCgatのバリデーション処理追加

--- app/Http/Controllers/Frontend/Chat/Api/StoreChatController.php
+++ app/Http/Controllers/Frontend/Chat/Api/StoreChatController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Frontend\Chat\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\Chat\StoreChatRequest;
 use App\UseCase\Frontend\Chat\Api\StoreChatUseCase;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
@@ -20,11 +21,13 @@ class StoreChatController extends Controller
     }
 
     /**
+     * @param StoreChatRequest $request
+     *
      * @return JsonResponse
      */
-    public function __invoke(): JsonResponse
+    public function __invoke(StoreChatRequest $request): JsonResponse
     {
-        $response = $this->storeChatUseCase->execute('テストクエッション', 'Man_Digest_v9');
+        $response = $this->storeChatUseCase->execute($request->getQuestion(), $request->getManualName());
 
         return response()->json($response->collect(), SymfonyResponse::HTTP_OK);
     }
