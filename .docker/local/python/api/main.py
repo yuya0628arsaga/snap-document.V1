e98982c8176bbff5c349c7209d595974b9904840
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-22 16:23:38 +0900

    「Chroma  requires sqlite3 >= 3.35.0.」のエラー解消コード追記

--- .docker/local/python/api/main.py
+++ .docker/local/python/api/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 
-from api.routers import chat
-from api.routers import chroma
-
+######################################################
+# 「RuntimeError: Your system has an unsupported version of sqlite3. Chroma  requires sqlite3 >= 3.35.0.」
+# 上記エラーの解消コード
+# このコードを他のモジュールより先に読み込む必要あり
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+######################################################
 
 app = FastAPI()
+
+from api.routers import chat
+from api.routers import chroma
 
 app.include_router(chat.router)
 app.include_router(chroma.router)
