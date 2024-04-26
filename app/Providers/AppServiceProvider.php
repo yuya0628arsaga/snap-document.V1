d778bb89d5a1d3b03f3e5145a4b7d4f8896178bf
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-26 12:13:22 +0900

    https化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::isProduction() || App::environment('develop', 'staging')) {
+            \URL::forceScheme('https');
+        }
     }
 }
