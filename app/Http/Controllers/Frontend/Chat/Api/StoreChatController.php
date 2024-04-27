90f001f5c4b308139c2e20c3d30b07cb8d4d1bdf
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-27 19:16:23 +0900

    StoreChatRequestを一旦削除

--- app/Http/Controllers/Frontend/Chat/Api/StoreChatController.php
+++ app/Http/Controllers/Frontend/Chat/Api/StoreChatController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Frontend\Chat\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Frontend\Chat\StoreChatRequest;
+// use App\Http\Requests\Frontend\Chat\StoreChatRequest;
 use App\Http\Resources\Frontend\Chat\StoreChatResource;
 use App\UseCase\Frontend\Chat\Api\StoreChatUseCase;
 use Illuminate\Http\JsonResponse;
@@ -22,16 +22,19 @@ class StoreChatController extends Controller
     }
 
     /**
-     * @param StoreChatRequest $request
+    //  * @param StoreChatRequest $request
      *
      * @return JsonResponse
      */
-    public function __invoke(StoreChatRequest $request): JsonResponse
+    public function __invoke(): JsonResponse
     {
         $response = $this->storeChatUseCase->execute(
-            $request->getQuestion(),
-            $request->getManualName(),
-            $request->getChatHistory(),
+            // $request->getQuestion(),
+            // $request->getManualName(),
+            // $request->getChatHistory(),
+            '質問1',
+            'Man_Digest_v9',
+            [],
         );
 
         return response()->json(new StoreChatResource($response), Response::HTTP_OK);
