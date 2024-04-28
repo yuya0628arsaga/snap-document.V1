a3e503f74be19a554fb2775a17ed7dc1f6066a7f
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-29 03:58:46 +0900

    その他不要なルーティング削除

--- routes/frontend/web.php
+++ routes/frontend/web.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +12,5 @@ use Illuminate\Support\Facades\Log;
 */
 
 Route::get('/', function () {
-    // $resp = Http::get('http://gpt_engine:8000/hello');
-    // $resp = $resp->json();
-    // Log::debug($resp);
     return view('frontend.home.index');
 });
