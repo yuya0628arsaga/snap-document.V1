17e534c25dcf89ddb3eb097ea7648feb46bdf0ef
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-04-29 04:00:23 +0900

    その他不要なルーティング削除

--- routes/frontend/api.php
+++ routes/frontend/api.php
@@ -5,11 +5,7 @@ declare(strict_types=1);
 use App\Http\Controllers\Frontend\Chat\Api\StoreChatController;
 use App\Models\Document;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Sleep;
 
 /*
 |--------------------------------------------------------------------------
