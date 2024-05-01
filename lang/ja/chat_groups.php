16c956e5f3cbc97fab50852dd05080878531e499
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-05-01 18:52:03 +0900

    chat_groupsテーブル追加

---
+++ lang/ja/chat_groups.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+$tableLabel = 'チャットグループ';
+
+return [
+    'table_description' => "${tableLabel}テーブル",
+    'id' => "${tableLabel}ID",
+    'title' => 'タイトル',
+    'last_chat_date' => '最後のチャットの日付',
+];
