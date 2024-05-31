08994588182bc5664b612b2c246ff93b11b5e80b
yuya0628arsaga <yuya.ogawa@arsaga.jp>
2024-05-31 13:41:07 +0900

    Google認証の異常系テストコード追加

--- tests/Feature/Frontend/Auth/GoogleAuthenticationTest.php
+++ tests/Feature/Frontend/Auth/GoogleAuthenticationTest.php
@@ -10,15 +10,14 @@ class GoogleAuthenticationTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-
-        // Socialiteのモックを設定
-        $this->mockSocialiteFacade();
     }
 
     /**
-     * モックの作成
+     * Socialiteのモックを設定
+     *
+     * @param bool $authSuccess
      */
-    protected function mockSocialiteFacade()
+    protected function mockSocialiteFacade($authSuccess = true)
     {
         $abstractUser = m::mock(ProviderUser::class);
         $abstractUser->shouldReceive('getId')->andReturn('123456789');
@@ -28,14 +27,16 @@ class GoogleAuthenticationTest extends TestCase
         $abstractUser->shouldReceive('getAvatar')->andReturn('https://test.com/avatar.jpg');
 
         $provider = m::mock(\Laravel\Socialite\Contracts\Provider::class);
-        $provider->shouldReceive('user')->andReturn($abstractUser);
+        $authSuccess
+        ? $provider->shouldReceive('user')->andReturn($abstractUser)
+        : $provider->shouldReceive('user')->andThrow(new \Exception('Google authentication failed'));
 
         $provider->shouldReceive('redirect')->andReturn(redirect('https://accounts.google.com/o/oauth2/auth'));
         Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
     }
 
     /**
-     * Googleログインボタンを押下したらGoogleの認証画面にリダイレクトする
+     * 正常系 - Googleログインボタンを押下したらGoogleの認証画面にリダイレクトする
      */
     public function test_google_redirect()
     {
@@ -44,19 +45,33 @@ class GoogleAuthenticationTest extends TestCase
     }
 
     /**
-     * ユーザーがGoogleで認証した場合、ログインさせる
+     * 正常系 - ユーザーがGoogleで認証した場合、ログインさせる
      */
     public function test_google_callback()
     {
+        $this->mockSocialiteFacade();
         $response = $this->get(route('user.auth.login.google.callback'));
 
         // 適切なリダイレクト先やユーザーの認証を確認
         $response->assertRedirect(route('user.home'));
         $this->assertAuthenticated();
+
         $this->assertDatabaseHas('users', [
             'name' => 'テスト',
             'email' => 'test@test.com',
             'avatar_url' => 'https://test.com/avatar.jpg',
         ]);
+    }
+
+    /**
+     * 異常系 - ユーザーがGoogle認証で失敗した場合、ログイン画面にリダイレクトさせる
+     */
+    public function test_google_callback_failure()
+    {
+        $this->mockSocialiteFacade(false);
+
+        $response = $this->get(route('user.auth.login.google.callback'));
+        $response->assertRedirect(route('user.auth.index'));
+        $this->assertGuest();
     }
 }
